02f25fff06a604b0cddca10e55098f44a05c6846
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-06 22:06:43 +0100

    update

--- authorization/userAuth.ts
+++ authorization/userAuth.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from "express";
+import { IAuthUser } from "../Models/AllInterfaces";
 
 export const isAdmin = async (
-  req: Request,
+  req: IAuthUser,
   res: Response,
   next: NextFunction
 ) => {
+  const user = req!.user;
+  const adminUser = user && user.role 
+  if (user.role === "admin") {
     
+  }
 };
