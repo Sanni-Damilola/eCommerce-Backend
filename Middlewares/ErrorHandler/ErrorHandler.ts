2f97082dd5554f1dbedb593f0644be5fa421dd82
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-13 11:36:38 +0100

    update

--- Middlewares/ErrorHandler/ErrorHandler.ts
+++ Middlewares/ErrorHandler/ErrorHandler.ts
@@ -1,15 +1,20 @@
 import { Response, Request, NextFunction } from "express";
 import { AppError } from "../../Utils/AppError";
 
-const DevErrorHandler = (err: AppError, res: Response) =>{
-    return res.status(err.httpCode).json({
-        httpCode: err.httpCode,
-        error: err,
-        message: err.message,
-        stack: err.stack
-    });
-}
+const DevErrorHandler = (err: AppError, res: Response) => {
+  return res.status(err.httpCode).json({
+    httpCode: err.httpCode,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+}; // recusive funtions
 
-export const ErrorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) =>{
-    DevErrorHandler(err, res)
-}
+export const ErrorHandler = (
+  err: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  DevErrorHandler(err, res);
+};
