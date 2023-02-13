984bdbc58bf7b4a53ea55792f6d5faa689ce4881
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-13 12:43:45 +0100

    update

--- Middlewares/Validation/UserValidation/UserSchema.ts
+++ Middlewares/Validation/UserValidation/UserSchema.ts
@@ -2,15 +2,15 @@ import Joi from "joi";
 
 // VALIDATION SCHEMA OBJECT
 
-export const UserSchemaValidation = ({
-    Register: Joi.object({
-        name: Joi.string().required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min(6).required(),
-        confirmPassword: Joi.string().required().valid(Joi.ref("password"))
-    }),
-    Login: Joi.object({
-        email: Joi.string().email().required(),
-        password: Joi.string().min(6).required(),
-    })
-})
+export const UserSchemaValidation = {
+  Register: Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+    confirmPassword: Joi.string().required().valid(Joi.ref("password")),
+  }),
+  Login: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  }),
+}; // requirement for signing Up
