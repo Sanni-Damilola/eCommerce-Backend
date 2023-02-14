9ff3394baab71762c922cc2f966428776dfbd178
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-14 21:20:50 +0100

    update

--- Middlewares/Validation/ProductsValidation/productSchema.ts
+++ Middlewares/Validation/ProductsValidation/productSchema.ts
@@ -2,4 +2,10 @@ import joi from "joi";
 
 //  Validation Schema for Product
 
-export const productSchemaValidation = {};
+export const productSchemaValidation = {
+  PostProduct: joi.object({
+    name: joi.string().required(),
+    price: joi.string().required(),
+    category: joi.string().required(),
+  }),
+} // requirement for posting product
