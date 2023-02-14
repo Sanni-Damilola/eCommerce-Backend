f34caf7571b7292c2b55d801973a816725f860f7
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-14 21:30:39 +0100

    update

--- Routes/productRoute.ts
+++ Routes/productRoute.ts
@@ -1,8 +1,8 @@
-import {Router} from "express"
-import { getAllProduct, postProduct } from "../Controllers/productController"
+import { Router } from "express";
+import { getAllProduct, postProduct } from "../Controllers/productController";
 
-const route = Router()
-route.route("/getAllProduct").get(getAllProduct)
-route.route("/createProduct").post(postProduct)
+const route = Router();
+route.route("/getAllProduct").get(getAllProduct);
+route.route("/createProduct").post(postProductValidation, postProduct);
 
-export default route
+export default route;
