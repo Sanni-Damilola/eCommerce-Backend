5cd05db2e46c0fa55c4f24b2f10fa18b9390b451
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-14 21:29:26 +0100

    update

--- Middlewares/Validation/ProductsValidation/producValidation.ts
+++ Middlewares/Validation/ProductsValidation/producValidation.ts
@@ -1,5 +1,11 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, RequestHandler } from "express";
 import { productSchemaValidation } from "./productSchema";
 import { validator } from "../Validator";
 
-
+export const postProductValidation: RequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  validator(productSchemaValidation.PostProduct, req.body, next);
+};
