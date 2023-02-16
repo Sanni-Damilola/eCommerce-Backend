0b86545d3f435cba9def66f15f1342845a3692cb
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-16 11:42:38 +0100

    update

--- Middlewares/Validation/UserValidation/UserValidation.ts
+++ Middlewares/Validation/UserValidation/UserValidation.ts
@@ -1,15 +1,15 @@
 import { NextFunction, Request, Response, RequestHandler } from "express";
 import { UserSchemaValidation } from "./UserSchema";
-import { validator } from "../Validator";
+import { validation } from "../Validator";
 
 export const RegisterValidation: RequestHandler = (
     req: Request, res: Response, next: NextFunction
 ) =>{
-    validator(UserSchemaValidation.Register, req.body, next)
+    validation(UserSchemaValidation.Register, req.body, next)
 };
 
 export const LoginValidation: RequestHandler = (
     req: Request, res: Response, next: NextFunction
 ) =>{
-    validator(UserSchemaValidation.Login, req.body, next)
+    validation(UserSchemaValidation.Login, req.body, next)
 }
--- Middlewares/Validation/Validator.ts
+++ Middlewares/Validation/Validator.ts
@@ -1,62 +1,30 @@
-// import { NextFunction } from "express";
-// import Joi from "joi";
-// import { AppError, HttpCode } from "../../Utils/AppError";
+import { NextFunction } from "express";
+import Joi from "joi";
+import { AppError, HttpCode } from "../../Utils/AppError";
 
 import { object } from "joi";
 
-// export const validation = (
-//   schemaName: Joi.ObjectSchema,
-//   body: object,
-//   next: NextFunction
-// ) => {
-//   const value = schemaName.validate(body, {
-//     allowUnknown: true,
-//     abortEarly: false,
-//     stripUnknown: true,
-//   });
+export const validation = (
+  schemaName: Joi.ObjectSchema,
+  body: object,
+  next: NextFunction
+) => {
+  const value = schemaName.validate(body, {
+    allowUnknown: true,
+    abortEarly: false,
+    stripUnknown: true,
+  });
 
-//   try {
-//     value.error
-//       ? next(
-//           new AppError({
-//             message: value.error.details[0].message,
-//             httpCode: HttpCode.UNPROCESSABLE_IDENTITY,
-//           })
-//         )
-//       : next();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-export enum HttpCode {
-  OK = 3000,
-}
-
-interface AppErrorAgrs {
-  name?: string;
-  message: string;
-  httpCode: HttpCode;
-  isOperational?: boolean;
-}
-
-export class AppError extends Error {
-  public readonly httpCode: HttpCode;
-  public readonly name: string;
-  public readonly isOperational: boolean = true;
-
-  constructor(args: AppErrorAgrs) {
-    super(args.message);
-
-    Object.setPrototypeOf(this, new.target.prototype);
-
-    this.name = args.name || "Error";
-    this.httpCode = args.httpCode;
-
-    if (args.isOperational !== undefined) {
-      this.isOperational = args.isOperational;
-    }
-
-    Error.captureStackTrace(this);
+  try {
+    value.error
+      ? next(
+          new AppError({
+            message: value.error.details[0].message,
+            httpCode: HttpCode.UNPROCESSABLE_IDENTITY,
+          })
+        )
+      : next();                                    
+  } catch (error) {
+    console.log(error);
   }
-}
+};
