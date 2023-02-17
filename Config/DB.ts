74c6a17b11e963c842b67b6d49594b91b9638e69
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-17 12:07:56 +0100

    update

--- Config/DB.ts
+++ Config/DB.ts
@@ -3,11 +3,11 @@ import { envVariables } from "./environmentVariables";
 
 const DB = envVariables.DB_STRING;
 
-export async function DBconnect(){
-    try {
-        const conn = await mongoose.connect(DB);
-        console.log(`Database is connected to ${conn.connection.host}`)
-    } catch (error) {
-        console.log(`An error occured in connecting to connection host`)
-    }
+export async function DBconnect() {
+  try {
+    const conn = await mongoose.connect(DB);
+    console.log(`Database is connected to ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`An error occured in connecting to connection host`);
+  }
 }
