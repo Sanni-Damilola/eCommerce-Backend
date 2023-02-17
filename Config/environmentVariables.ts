0d54d7acedf0399c29b617bde7320cbbe61f0e17
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-17 10:14:33 +0100

    update

--- Config/environmentVariables.ts
+++ Config/environmentVariables.ts
@@ -7,3 +7,8 @@
 //   DB_STRING: process.env.MONGODB_STRING as string,
 //   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY as string
 // };
+import dotenv from "domain";
+
+export const envVariables = {
+  PORT: process.env.PORT as string,
+};
